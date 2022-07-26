76ca3f125b8c1f646c2a1a568dfefd24fdb52baa
Erick <josiels88@gmail.com>
2022-07-26 13:21:22 -0500

    the project is added

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+import type {Node} from 'react';
+import {
+  Text,
+  View, 
+} from 'react-native';
+
+const App = () => {
+
+useEffect(() => {
+
+}, [])
+
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  );
+};
+
+export default App;
